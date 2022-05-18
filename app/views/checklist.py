48d56493b1299a1a4ba41b8ad64b82ba2a5c771f
Annetkhavere <anneywan5@gmail.com>
2022-05-18 22:36:25 +0300

    add the borrowing function

--- app/views/checklist.py
+++ app/views/checklist.py
@@ -20,3 +20,34 @@ def transactions_page():
                             members_can_borrow = members_can_borrows, 
                             books_to_return = books_to_return)
 
+                            
+@app.route('/borrow-book', methods=['GET','POST'])
+def borrow_book():
+    # reads the form values
+    member_requested = request.form.get("member_name")
+    book_requested = request.form.get("book_name")
+    member = Member.query.get(int(member_requested))
+    book = Book.query.get(int(book_requested))
+
+    if(book and member):
+        member.to_pay = member.to_pay + 30
+        book.borrow_stock = book.borrow_stock - 1
+        book.member_count = book.member_count + 1
+        borrow = Book_borrowed(book = book.id,
+                               member = member.id)
+        borrow_book = Transaction(book_name = book.title,
+                                  member_name = member.member_name,
+                                  type_of_transaction = "borrow",
+                                  amount = 0,
+                                  date= date.today())
+        db.session.add(borrow_book)
+        db.session.add(borrow)
+        db.session.commit()
+        flash(f"Issued book", category='success')
+
+    else:
+        flash(f'Enter the Value', category = 'danger')
+
+    return redirect(request.referrer)
+
+
