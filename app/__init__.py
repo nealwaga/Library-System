6b7f18af89ec3ed27eb3b0cd0fae0cc9b7260b7b
nealwaga <waganeal@gmail.com>
2022-05-19 11:45:17 +0300

    creation of secret key

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ ENV ='dev'
 
 if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
-    app.config['SECRET_KEY'] = ''
+    app.config['SECRET_KEY'] = '536c8955584707f53fb23f1ebfa33879'
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQL_DATABASE_URL']
     app.config['SECRET_KEY'] = os.environ['SECRET_KEY'] 
